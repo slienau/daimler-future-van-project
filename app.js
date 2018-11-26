d46f96fbadad0f0d7b09881e5959de419e86e46e
berlinbeachboy <45067029+berlinbeachboy@users.noreply.github.com>
2018-11-26 22:06:00 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+const mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var indexRouter = require('./routes/index');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// view engine setup
+
+mongoose
+    .connect(
+        'mongodb://mongo:27017/PAS-Backend',
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
+module.exports = app;
