8f9a385e1d5bdf87f276433b94f6c3fc92ba5965
Christoph Witzko <github@christophwitzko.com>
2018-11-28 16:11:21 +0100

    feat: update react-navigator to version 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import {createSwitchNavigator, createAppContainer} from 'react-navigation'
+
 import LoadingScreen from './views/LoadingScreen'
 import Login from './views/Login'
 import Welcome from './views/Welcome'
 import Account from './views/Account'
 import Games from './views/Games'
 import Information from './views/Information'
-
-import {createSwitchNavigator} from 'react-navigation'
 
 const MainView = createSwitchNavigator(
   {
@@ -20,19 +19,17 @@ const MainView = createSwitchNavigator(
   }
 )
 
-const RootNavigator = createSwitchNavigator(
-  {
-    LoadingScreen,
-    Login,
-    MainView,
-  },
-  {
-    initialRouteName: 'LoadingScreen',
-  }
+const App = createAppContainer(
+  createSwitchNavigator(
+    {
+      LoadingScreen,
+      Login,
+      MainView,
+    },
+    {
+      initialRouteName: 'LoadingScreen',
+    }
+  )
 )
-
-const App = () => {
-  return <RootNavigator />
-}
 
 export default App
