661e550131b08864fa87c936d7ccddd3505950ac
Christoph Witzko <github@christophwitzko.com>
2018-12-02 23:39:06 +0100

    feat: move redux provider to app and init state store async

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 /** @format */
 
-import React from 'react'
 import {AppRegistry} from 'react-native'
-import {Provider} from 'react-redux'
 import App from './src/App'
-import configureStore from './src/init/store'
 import {name as appName} from './app.json'
 
-const store = configureStore()
-
-const RNRedux = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
-
-AppRegistry.registerComponent(appName, () => RNRedux)
+AppRegistry.registerComponent(appName, () => App)
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import {createSwitchNavigator, createAppContainer} from 'react-navigation'
+import React from 'react'
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import {Provider} from 'react-redux'
 
 import LoadingScreen from './views/LoadingScreen'
 import Login from './views/Login'
@@ -6,6 +8,7 @@ import Welcome from './views/Welcome'
 import Account from './views/Account'
 import Games from './views/Games'
 import Information from './views/Information'
+import configureStore from './init/store'
 
 const MainView = createSwitchNavigator(
   {
@@ -19,7 +22,7 @@ const MainView = createSwitchNavigator(
   }
 )
 
-const App = createAppContainer(
+const AppNavigator = createAppContainer(
   createSwitchNavigator(
     {
       LoadingScreen,
@@ -32,4 +35,23 @@ const App = createAppContainer(
   )
 )
 
-export default App
+export default class App extends React.Component {
+  state = {
+    store: null,
+  }
+
+  async componentDidMount() {
+    const store = await configureStore()
+    this.setState({store})
+  }
+
+  render() {
+    const {store} = this.state
+    if (store === null) return null
+    return (
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    )
+  }
+}
