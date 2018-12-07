addc3a99976ec22249d836f68944da1cb313e564
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2018-12-07 18:21:31 +0100

    add redux devtools

--- src/init/store.js
+++ src/init/store.js
@@ -1,12 +1,18 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './rootReducer'
 
 let store = null
 
+let composeEnhancers = compose
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 async function configureStore() {
-  store = createStore(rootReducer, applyMiddleware(thunk))
+  store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
   return store
 }
 
