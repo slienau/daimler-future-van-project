c2d669d066642c2f96dcbf9de0a44d57fdbe91cf
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2018-12-12 15:39:53 +0100

    fix render error on welcome screen

--- src/views/Welcome/index.js
+++ src/views/Welcome/index.js
@@ -19,7 +19,6 @@ import styled from 'styled-components/native'
 
 // For bottom button
 const StyledButton = styled(Button)`
-  alignself: center;
   position: absolute;
   left: 30%;
   right: 30%;
