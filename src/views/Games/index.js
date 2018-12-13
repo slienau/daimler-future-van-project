abc5b93b013c76acf9737f40fc9c5144d0918646
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2018-12-13 17:34:35 +0100

    add header to games view

--- src/views/Games/index.js
+++ src/views/Games/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 
 import styled from 'styled-components/native'
-import {
-  Container,
-  Content,
-  Button,
-  Footer,
-  FooterTab,
-  Icon,
-  Text,
-} from 'native-base'
+import {Container} from 'native-base'
+import ViewHeader from '../../components/ViewHeader'
 
 const StyledView = styled.View`
   flex: 1;
@@ -19,36 +12,11 @@ const StyledView = styled.View`
 const Games = props => {
   return (
     <StyledView>
-      <Text>Games View</Text>
       <Container>
-        <Content />
-
-        <Footer>
-          <FooterTab>
-            <Button
-              vertical
-              onPress={() => props.navigation.navigate('Account')}>
-              <Icon name="person" />
-              <Text>Account</Text>
-            </Button>
-            <Button
-              vertical
-              onPress={() => props.navigation.navigate('Welcome')}>
-              <Icon name="map" />
-              <Text>Navigate</Text>
-            </Button>
-            <Button vertical active>
-              <Icon active name="apps" />
-              <Text>Games</Text>
-            </Button>
-            <Button
-              vertical
-              onPress={() => props.navigation.navigate('Information')}>
-              <Icon name="information" />
-              <Text>Van Info</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
+        <ViewHeader
+          title="Games"
+          onMenuPress={() => props.navigation.openDrawer()}
+        />
       </Container>
     </StyledView>
   )
