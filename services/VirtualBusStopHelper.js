a765d69965684026ab1f4a747168c60589c87b5f
philipp <ratz.phil@gmail.com>
2018-12-13 17:27:58 +0100

    fixed db filling skriptssss

--- services/VirtualBusStopHelper.js
+++ services/VirtualBusStopHelper.js
@@ -3,32 +3,26 @@ const VirtualBusStop = require('../models/VirtualBusStop.js')
 class VirtualBusStopHelper {
   // Check if any users are there and if not create two static users
   static async setupVBs () {
-    let setupNeeded = false
-    await VirtualBusStop.find({},
-      function (error, items) {
-        if (error || items === null || items.length === 0) {
-          setupNeeded = true
-        }
-      })
-    if (setupNeeded) {
-      const zoo = new VirtualBusStop({
-        location: {
-          latitude: 52.507304,
-          longitude: 13.330626
-        },
-        accessible: true
-      })
-      const potsdamerPl = new VirtualBusStop({
-        location: {
-          latitude: 52.509726,
-          longitude: 13.376962
-        },
-        accessible: true
-      })
+    const vbs = await VirtualBusStop.find({})
+    if (vbs !== null && vbs.length !== 0) return
 
-      await zoo.save()
-      await potsdamerPl.save()
-    }
+    const zoo = new VirtualBusStop({
+      location: {
+        latitude: 52.507304,
+        longitude: 13.330626
+      },
+      accessible: true
+    })
+    const potsdamerPl = new VirtualBusStop({
+      location: {
+        latitude: 52.509726,
+        longitude: 13.376962
+      },
+      accessible: true
+    })
+
+    await zoo.save()
+    await potsdamerPl.save()
   }
 }
 
