98e920a87ee16bbcd61c95e43702d046deeec6a0
Antonio <antonio.schuerer@gmail.com>
2019-01-07 13:05:21 +0100

    AWS EC2 IP added

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo "triggering deploy..."
-scp scripts/deploy.sh azure-user@40.89.153.255:~/
-ssh azure-user@40.89.153.255 /home/azure-user/deploy.sh
+scp scripts/deploy.sh ubuntu@3.120.249.73:~/
+ssh ubuntu@3.120.249.73 /home/ubuntu/deploy.sh
