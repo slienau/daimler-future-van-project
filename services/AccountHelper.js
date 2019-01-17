a87ddeebb00e83d3b73b2fec1ded0b4544aa9254
Antonio Schürer <antonio.schuerer@gmail.com>
2019-01-17 11:39:15 +0100

    account for each team member

--- services/AccountHelper.js
+++ services/AccountHelper.js
@@ -31,8 +31,106 @@ class AccountHelper {
       }
     })
 
+    const christoph = new Account({
+      username: 'christoph',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'christophiscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
+    const alex = new Account({
+      username: 'alex',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'alexiscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
+    const sebastian = new Account({
+      username: 'sebastian',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'sebastianiscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
+    const domenic = new Account({
+      username: 'domenic',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'domeniciscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
+    const marius = new Account({
+      username: 'marius',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'mariusiscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
+    const philipp = new Account({
+      username: 'philipp',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'philippiscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
+    const antonio = new Account({
+      username: 'antonio',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'antonioiscooler',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
+
     await admin.save()
     await maxUser.save()
+    await christoph.save()
+    await sebastian.save()
+    await alex.save()
+    await domenic.save()
+    await marius.save()
+    await philipp.save()
+    await antonio.save()
   }
 }
 
