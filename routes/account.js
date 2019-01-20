4ef1a14525118c19d688c6a1c39fc9e82c0c5999
Antonio <schuerer@campus.tu-berlin.de>
2019-01-20 16:19:50 +0100

    co2savings in account, order and leaderboard

--- routes/account.js
+++ routes/account.js
@@ -8,6 +8,7 @@ router.get('/', async function (req, res) {
   let account, bonusitem, responsebody
   let bonuspoints = 0
   let co2savings = 0
+  let distance = 0
   try {
     account = await Account.findById(req.user._id, '-password')
     console.log('Requested Account with id ' + account._id)
@@ -23,19 +24,22 @@ router.get('/', async function (req, res) {
           {
             _id: '$accountId',
             bonuspoints: { $sum: { $multiply: ['$distance', '$bonusMultiplier'] } },
-            co2savings: { $sum: '$co2savings' }
+            co2savings: { $sum: '$co2savings' },
+            distance: { $sum: '$distance' }
           }
       }
     ])
     if (bonusitem.length > 0) {
       bonuspoints = bonusitem[0].bonuspoints
       co2savings = bonusitem[0].co2savings
+      distance = bonusitem[0].distance
     }
     bonuspoints = Number(bonuspoints.toFixed(0))
     co2savings = Number(co2savings.toFixed(2))
     responsebody = account.toObject()
     responsebody.bonuspoints = bonuspoints
     responsebody.co2savings = co2savings
+    responsebody.distance = distance
     res.json(responsebody)
   } catch (error) {
     res.status(404).json({ error: error, msg: 'No items found' })
