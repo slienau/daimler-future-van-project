5989f46711ee091f2b03a59893d25cca85a220f6
Antonio <schuerer@campus.tu-berlin.de>
2019-01-28 21:36:34 +0100

    usernames in leaderboard

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Order = require('../models/Order.js')
+const Account = require('../models/Account.js')
 
 router.get('/', async function (req, res) {
   res.setHeader('Content-Type', 'application/json')
@@ -15,18 +16,25 @@ router.get('/', async function (req, res) {
               loyaltyPoints: { $sum: { $multiply: ['$distance', '$bonusMultiplier'] } },
               co2savings: { $sum: '$co2savings' }
             }
-        },
-        {
-          $project: {
-            _id: 0
-          }
         }
       ]
     )
       .sort({ loyaltyPoints: -1 }) // sort the scoreboard by loyaltyPoints desc
       .limit(10) // return only the first ten in the leaderboard
-    res.json(leaderboard)
+    let leaderboardWithUsernames = leaderboard.slice()
+    let account
+    for (let i = 0; i < leaderboard.length; i++) {
+      account = String(leaderboard[i]._id)
+      if (String(leaderboard[i]._id)) {
+        account = await Account.findById(String(leaderboard[i]._id)).lean()
+        delete leaderboardWithUsernames[i]._id
+        leaderboardWithUsernames[i].username = account.username
+        leaderboardWithUsernames[i].co2savings = Number(leaderboardWithUsernames[i].co2savings.toFixed(2))
+      }
+    }
+    res.json(leaderboardWithUsernames)
   } catch (error) {
+    console.log(error)
     res.status(404).json({ error: error, description: 'No items found' })
   }
 })
