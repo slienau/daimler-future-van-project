a61eb6b5b5a36642485f18fc1053a9f7b4f6a18a
Christoph Witzko <github@christophwitzko.com>
2019-01-31 10:38:49 +0100

    chore: log network calls in RNDebugger

--- index.js
+++ index.js
@@ -4,4 +4,27 @@ import {AppRegistry} from 'react-native'
 import App from './src/init/App'
 import {name as appName} from './app.json'
 
+global.XMLHttpRequest = global.originalXMLHttpRequest
+  ? global.originalXMLHttpRequest
+  : global.XMLHttpRequest
+global.FormData = global.originalFormData
+  ? global.originalFormData
+  : global.FormData
+
+fetch // eslint-disable-line no-unused-expressions
+
+if (window.__FETCH_SUPPORT__) {
+  // it's RNDebugger only to have
+  window.__FETCH_SUPPORT__.blob = false
+} else {
+  /*
+   * Set __FETCH_SUPPORT__ to false is just work for `fetch`.
+   * If you're using another way you can just use the native Blob and remove the `else` statement
+   */
+  global.Blob = global.originalBlob ? global.originalBlob : global.Blob
+  global.FileReader = global.originalFileReader
+    ? global.originalFileReader
+    : global.FileReader
+}
+
 AppRegistry.registerComponent(appName, () => App)
