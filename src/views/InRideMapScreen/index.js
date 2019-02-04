f5afcacb2d268fe28ba292537bbfc3e5b5421657
Christoph Witzko <github@christophwitzko.com>
2019-02-04 22:35:01 +0100

    feat(InRideMapScreen): default location to van position

--- src/views/InRideMapScreen/index.js
+++ src/views/InRideMapScreen/index.js
@@ -14,10 +14,9 @@ const StyledMapView = styled(MapView)`
 
 const InRideMapScreen = props => {
   const mapRegion = {
-    latitude: _.get(props.route, 'vanStartVBS.location.latitude'),
-    longitude: _.get(props.route, 'vanStartVBS.location.longitude'),
     latitudeDelta: 0.02,
     longitudeDelta: 0.02,
+    ..._.get(props.activeOrderStatus, 'vanLocation'),
   }
   return (
     <Container>
@@ -33,9 +32,9 @@ const InRideMapScreen = props => {
 }
 
 InRideMapScreen.propTypes = {
-  route: PropTypes.object,
+  activeOrderStatus: PropTypes.object,
 }
 
 export default connect(state => ({
-  route: _.get(state.map, 'routes.0'),
+  activeOrderStatus: state.orders.activeOrderStatus,
 }))(InRideMapScreen)
