8c7b55d0d09aa382bc8fa09331467a501b7d329f
adittmann <alexander.dittmann@campus.tu-berlin.de>
2019-02-07 12:49:31 +0100

    Revert "Loyalty and Leaderboard Improvements "

--- services/AccountHelper.js
+++ services/AccountHelper.js
@@ -7,26 +7,51 @@ class AccountHelper {
       return null
     }
     let status
-    if (loyaltyPoints > 300) {
+    if (loyaltyPoints > 150) {
       status = 'gold'
-    } else if (loyaltyPoints > 150) {
+    } else if (loyaltyPoints > 100) {
       status = 'silver'
-    } else {
+    } else if (loyaltyPoints > 50) {
       status = 'bronze'
     }
     return status
   }
   // Check if any users are there and if not create two static users
   static async setUpAccounts () {
-    const accs = await Account.findOne({ 'username': 'christoph' })
-    if (accs !== null) return (console.log(accs))
+    const accs = await Account.find({ 'username': 'admin' })
+    if (accs !== null && accs.length !== 0) return (console.log(accs))
+
+    const admin = new Account({
+      username: 'admin',
+      firstName: 'admin',
+      lastName: 'admin',
+      password: 'adminiscooler',
+      address: {
+        street: 'adminstreet 1',
+        zipcode: 10000,
+        city: 'Berlin'
+      },
+      email: 'ad@min.admin'
+    })
+    const maxUser = new Account({
+      username: 'maexle',
+      firstName: 'Max',
+      lastName: 'Müller',
+      password: 'maxiscool',
+      email: 'max@max.max',
+      address: {
+        street: 'Salzufer 1',
+        zipcode: 10587,
+        city: 'Berlin'
+      }
+    })
 
     const christoph = new Account({
       username: 'christoph',
-      firstName: 'Christoph',
-      lastName: 'Witzko',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'christophiscooler',
-      email: 'christoph.witzko@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -36,10 +61,10 @@ class AccountHelper {
 
     const alex = new Account({
       username: 'alex',
-      firstName: 'Alex',
-      lastName: 'Dittmann',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'alexiscooler',
-      email: 'alex.dittmann@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -49,10 +74,10 @@ class AccountHelper {
 
     const sebastian = new Account({
       username: 'sebastian',
-      firstName: 'Sebastian',
-      lastName: 'Lienau',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'sebastianiscooler',
-      email: 'sebastian.lienau@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -62,10 +87,10 @@ class AccountHelper {
 
     const domenic = new Account({
       username: 'domenic',
-      firstName: 'Domenic',
-      lastName: 'Bosin',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'domeniciscooler',
-      email: 'domenic.bosin@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -75,10 +100,10 @@ class AccountHelper {
 
     const marius = new Account({
       username: 'marius',
-      firstName: 'Marius',
-      lastName: 'Möck',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'mariusiscooler',
-      email: 'marius.moeck@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -88,10 +113,10 @@ class AccountHelper {
 
     const philipp = new Account({
       username: 'philipp',
-      firstName: 'Philipp',
-      lastName: 'Ratz',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'philippiscooler',
-      email: 'philipp.ratz@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -101,10 +126,10 @@ class AccountHelper {
 
     const antonio = new Account({
       username: 'antonio',
-      firstName: 'Antonio',
-      lastName: 'Schürer',
+      firstName: 'Max',
+      lastName: 'Müller',
       password: 'antonioiscooler',
-      email: 'antonio.schuerer@tu-berlin.de',
+      email: 'max@max.max',
       address: {
         street: 'Salzufer 1',
         zipcode: 10587,
@@ -112,6 +137,8 @@ class AccountHelper {
       }
     })
 
+    await admin.save()
+    await maxUser.save()
     await christoph.save()
     await sebastian.save()
     await alex.save()
