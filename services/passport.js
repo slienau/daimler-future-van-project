770cf1a3a8c9cdd5e622e6398bf4a1daba533023
philipp <ratz.phil@gmail.com>
2019-02-07 21:03:25 +0100

    small refactors and fixes

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,7 @@ function (username, password, cb) {
   return Account.findOne({ 'username': username, 'password': password })
     .then(user => {
       if (!user) {
-        return cb(null, false, { message: 'Incorrect email or password.' })
+        return cb(null, false, { message: 'Incorrect username or password.' })
       }
 
       return cb(null, user, {
