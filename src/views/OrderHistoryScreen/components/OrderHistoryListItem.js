644143961c4c71c9984abff83e09d27273b1e711
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-02-08 12:36:29 +0100

    OrderHistory: align from/to

--- src/views/OrderHistoryScreen/components/OrderHistoryListItem.js
+++ src/views/OrderHistoryScreen/components/OrderHistoryListItem.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Text, ListItem, Right, Icon, Body} from 'native-base'
+import {Text, ListItem, Right, Icon, Body, View} from 'native-base'
 import {StyleSheet} from 'react-native'
+import DefaultText from '../../../components/UI/DefaultText'
 
 const OrderHistoryListItem = props => {
   let bodyText = 'Order has been canceled'
   if (!props.order.canceled) {
     bodyText = (
-      <Text>
-        From: {props.order.vanStartVBS.name} {'\n'}
-        To: {props.order.vanEndVBS.name}
-      </Text>
+      <View>
+        <View style={styles.row}>
+          <Text style={styles.leftColumn}>From:</Text>
+          <Text style={styles.rightColumn}>{props.order.vanStartVBS.name}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.leftColumn}>To:</Text>
+          <Text style={styles.rightColumn}>{props.order.vanEndVBS.name}</Text>
+        </View>
+      </View>
     )
   }
   return (
@@ -20,17 +27,14 @@ const OrderHistoryListItem = props => {
         if (!props.order.canceled) props.onItemPress()
       }}>
       <Body>
-        <Text>
-          <Text style={styles.time}>
+        <View>
+          <DefaultText style={styles.time}>
             {props.order.orderTime.format('L, LT')}
-            {'\n'}
-          </Text>
+          </DefaultText>
           {bodyText}
-        </Text>
+        </View>
       </Body>
-      <Right>
-        <Icon name="arrow-forward" />
-      </Right>
+      <Right>{!props.order.canceled && <Icon name="arrow-forward" />}</Right>
     </ListItem>
   )
 }
@@ -43,6 +47,16 @@ OrderHistoryListItem.propTypes = {
 const styles = StyleSheet.create({
   time: {
     fontWeight: 'bold',
+    paddingBottom: 5,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  leftColumn: {
+    flex: 1,
+  },
+  rightColumn: {
+    flex: 3,
   },
 })
 
