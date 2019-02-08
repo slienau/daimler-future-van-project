50d1f5fa96cf18d1eb977ab96e9f383955266be4
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-02-08 15:36:39 +0100

    use styled-components in DefaultText component

--- src/components/UI/DefaultText.js
+++ src/components/UI/DefaultText.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import {StyleSheet} from 'react-native'
+import styled from 'styled-components/native/dist/styled-components.native.esm'
 import {Text} from 'native-base'
 import {DARK_COLOR, GREY_COLOR} from './colors'
 
-const DefaultText = props => {
-  const styles = StyleSheet.create({
-    text: {
-      fontSize: 16,
-      color: props.greyColor ? GREY_COLOR : DARK_COLOR,
-    },
-  })
+const StyledText = styled(Text)`
+  color: ${props => (props.greyColor ? GREY_COLOR : DARK_COLOR)};
+`
 
+const DefaultText = props => {
   return (
-    <Text {...props} style={[styles.text, props.style]}>
+    <StyledText {...props} style={[styles.text, props.style]}>
       {props.children}
-    </Text>
+    </StyledText>
   )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+  },
+})
 
 export default DefaultText
