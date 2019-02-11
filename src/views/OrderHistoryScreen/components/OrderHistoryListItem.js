2a29e509b32d9371dd947c7478dad61e9cd3abc6
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-02-11 12:40:27 +0100

    bugfix: render cancel order text inside text component

--- src/views/OrderHistoryScreen/components/OrderHistoryListItem.js
+++ src/views/OrderHistoryScreen/components/OrderHistoryListItem.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Text, ListItem, Right, Icon, Body, View} from 'native-base'
+import {ListItem, Right, Icon, Body, View} from 'native-base'
 import {StyleSheet} from 'react-native'
 import DefaultText from '../../../components/UI/DefaultText'
 
 const OrderHistoryListItem = props => {
-  let bodyText = 'Order has been canceled'
+  let bodyText = <DefaultText>Order has been canceled</DefaultText>
   if (!props.order.canceled) {
     bodyText = (
       <View>
         <View style={styles.row}>
-          <Text style={styles.leftColumn}>From:</Text>
-          <Text style={styles.rightColumn}>{props.order.vanStartVBS.name}</Text>
+          <DefaultText style={styles.leftColumn}>From:</DefaultText>
+          <DefaultText style={styles.rightColumn}>
+            {props.order.vanStartVBS.name}
+          </DefaultText>
         </View>
         <View style={styles.row}>
-          <Text style={styles.leftColumn}>To:</Text>
-          <Text style={styles.rightColumn}>{props.order.vanEndVBS.name}</Text>
+          <DefaultText style={styles.leftColumn}>To:</DefaultText>
+          <DefaultText style={styles.rightColumn}>
+            {props.order.vanEndVBS.name}
+          </DefaultText>
         </View>
       </View>
     )
   }
   return (
     <ListItem
+      style={styles.listItem}
       button
       onPress={() => {
         if (!props.order.canceled) props.onItemPress()
@@ -57,6 +62,10 @@ const styles = StyleSheet.create({
   },
   rightColumn: {
     flex: 3,
+  },
+  listItem: {
+    paddingTop: 5,
+    paddingBottom: 10,
   },
 })
 
